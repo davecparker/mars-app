c3a20b630bf60f2a1232eb825950394543aa42ad
davecparker <dcparker65@gmail.com>
2015-11-06 19:09:56 -0800

    Basic greenhouse game
    Basic greenhouse game, uses water to make food

--- greenhouse.lua
+++ greenhouse.lua
@@ -24,7 +24,35 @@ local timerID  -- repeating timer
 
 -- Called when the drops are done animating
 function dropsDone( drops )
+	-- Water increases the plant's health
+	drops.plant.health = game.pinValue( drops.plant.health + 20, 0, maxHealth )
 	drops:removeSelf()
+end
+
+-- Water the plant
+function waterPlant( plant )
+	-- Make a group of 3 drops
+	local drops = act:newGroup()
+	drops.x = plant.x
+	drops.y = plant.y - 60  -- will animate down to the touch point
+	local dropSize = 12
+	act:newImage( "drop.png", { parent = drops, x = 3, y = -4, width = dropSize } )
+	act:newImage( "drop.png", { parent = drops, x = -3, y = -8, width = dropSize } )
+	act:newImage( "drop.png", { parent = drops, x = 0, y = 0, width = dropSize } )
+	drops.plant = plant
+	game.addWater( -1 )
+
+	-- Animate the drops falling
+	transition.to( drops, { y = plant.y - 20, xScale = 2, yScale = 2,
+			time = 500, onComplete = dropsDone } )
+end
+
+-- Remove the plant
+function removePlant( plant )
+	if plant.flowers then
+		plant.flowers:removeSelf()
+	end
+	plant:removeSelf()
 end
 
 -- Touch handler for plants
@@ -32,82 +60,94 @@ function touchPlant( event )
 	if event.phase == "began" then
 		local plant = event.target
 
-		-- Tapping on a plant with fruit harvests it
-		if plant.fruit then
-			plant.fruit:removeSelf()
-			plant.fruit = nil
-			plant.health = 0   -- plant is taken for food
-			game.addFood( 10 )
-		end
-
-		-- Tapping on a dead plant removes it
+		-- Process plant based on its state
 		if plant.health <= 0 then
-			plant:removeSelf()
-			return true
+			removePlant( plant )    -- Remove dead plant
+		elseif plant.flowers then
+			-- Harvest this plant
+			removePlant( plant )
+			game.addFood( 10 )
+			game.messageBox( "10 kg of food")
+		elseif game.water() <= 0 then
+			game.messageBox( "Out of water!" )
+		else
+			waterPlant( plant )
 		end
-
-		-- Water improves the plant's health
-		plant.health = game.pinValue( plant.health + 20, 0, maxHealth )
-
-		-- Make a group of 3 drops
-		local drops = act:newGroup()
-		drops.x = event.x
-		drops.y = event.y - 40  -- will animate down to the touch point
-		local dropSize = 12
-		act:newImage( "drop.png", { parent = drops, x = 3, y = -4, width = dropSize } )
-		act:newImage( "drop.png", { parent = drops, x = -3, y = -8, width = dropSize } )
-		act:newImage( "drop.png", { parent = drops, x = 0, y = 0, width = dropSize } )
-
-		-- Animate the drops falling
-		transition.to( drops, { y = event.y, xScale = 2, yScale = 2,
-				time = 500, onComplete = dropsDone } )
 	end
 	return true
+end
+
+-- Make a new plant at the given location
+function makePlant( x, y )
+	local size = 40
+	local plant = act:newImage( "plant.png", { parent = plants, height = size } )
+	plant.x = x
+	plant.y = y + size / 2
+	plant.anchorY = 1   -- grow from bottom up
+	plant:setFillColor( 0, 0.6, 0 )
+	plant.health = maxHealth
+	plant:addEventListener( "touch", touchPlant )
 end
 
 -- Touch handler for the dirt background
 function touchDirt( event )
 	if event.phase == "began" then
-		local plant = act:newImage( "plant.png", { parent = plants, width = 40 } )
-		plant.x = event.x
-		plant.y = event.y
-		plant:setFillColor( 0, 0.6, 0 )
-		plant.health = maxHealth
-		plant.age = 0
-		plant:addEventListener( "touch", touchPlant )
+		makePlant( event.x, event.y )
 	end
 	return true
 end
 
--- Called for each timer tick
-function timerTick()
-	-- Grow/age all the plants
-	for i = 1, plants.numChildren do
-		local plant = plants[i]
-		plant.age = plant.age + 1
-		plant.health = game.pinValue( plant.health - 1, 0, maxHealth )
-
+-- Set a plant's color based on its health
+function adjustPlantColor( plant )
+	if plant.health <= 0 then
+		-- Color dead plants dark brown and remove flowers
+		plant:setFillColor( 0.5, 0.25, 0 )
+		if plant.flowers then
+			plant.flowers:removeSelf()
+		end
+	else
 		-- Interpolate plant color based on health
-		local deadColor = { r = 0.5, g = 0.4, b = 0.2 }  -- brown
-		local goodColor = { r = 0.0, g = 0.6, b = 0.0 }  -- green
+		local deadColor = { r = 0.5, g = 0.4, b = 0.0 }   -- brown
+		local goodColor = { r = 0.0, g = 0.6, b = 0.0 }   -- green
 		local hf = plant.health / 100
 		local ihf = 1 - hf
 		local r = ihf * deadColor.r + hf * goodColor.r 
 		local g = ihf * deadColor.g + hf * goodColor.g 
 		local b = ihf * deadColor.b + hf * goodColor.b 
-		plant:setFillColor( r, g, b )  
+		plant:setFillColor( r, g, b )
+		-- TODO: wither flowers  
+	end
+end
 
-		-- Mature healthy plants make fruit
-		if plant.age > 120 and plant.health > 70 then
-			if not plant.fruit then
-				plant.fruit = act:newImage( "fruit.png", { width = 40,
-									x = plant.x, y = plant.y + 20 } )
-			end
-		end
+-- Add flowers to the plant if it doesn't already have them
+function addFlowers( plant )
+	if not plant.flowers then
+		local flowers = act:newGroup()
+		flowers.x = plant.x
+		flowers.y = plant.y
+		local size = 25
+		act:newImage( "flower.png", { parent = flowers, width = size, x = 0, y = -50 } )
+		act:newImage( "flower.png", { parent = flowers, width = size, x = 20, y = -30} )
+		act:newImage( "flower.png", { parent = flowers, width = size, x = -10, y = -20 } )
+		plant.flowers = flowers
+	end
+end
 
-		-- Healthy plants grow (up to a certain age)
-		if plant.age < 100 and plant.health > 50 then
-			local newScale = plant.xScale * 1.01
+-- Called for each timer tick
+function timerTick()
+	-- Grow/adjust all the plants
+	for i = 1, plants.numChildren do
+		-- Time decreases plant health due to need for more water
+		local plant = plants[i]
+		plant.health = game.pinValue( plant.health - 1, 0, maxHealth )
+		adjustPlantColor( plant )
+
+		if plant.xScale >= 3 and plant.health >= 70 then
+			-- Large, healthy plants make flowers and are ready to pick for food
+			addFlowers( plant )
+		elseif plant.xScale < 3 and plant.health > 50 then
+			-- Healthy plants grow (up to a certain size)
+			local newScale = plant.xScale * 1.02
 			transition.cancel( plant )
 			transition.to( plant, { xScale = newScale, yScale = newScale, time = msTimer } )
 		end
