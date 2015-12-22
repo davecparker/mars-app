9a041357fbd7046ecabce60b5b4326c48f63662a
electriccord <c_lamphere@sbcglobal.net>
2015-12-22 15:05:41 -0800

    intermediate version to get to desk top

--- thrustNav.lua
+++ thrustNav.lua
@@ -28,6 +28,15 @@ local act = game.newAct()
 --   2) use two space images (one with Mars, one with Earth) for left and right
 --   3) detect and stop vertical up/down with equal reverse thrust
 
+-- fix text to be clean(horz/vert degrees and velocity) with columns and no "getting close" messages
+-- eliminate text impulses line
+-- add print of list of children to Act.lua in scene:show did area
+-- add texture memory info to debug screen
+-- fix close - ontarget to be just on-target
+-- remove vertical and horizontal white lines 
+-- fix target orbit circle disappearing when spin 360
+
+
 ------------------------- Start of Activity --------------------------------
 
 -- File local variables
@@ -60,6 +69,9 @@ local thrusterSound     -- thrust sound
 local leftAccelerate, rightAccelerate, upAccelerate, downAccelerate
 local accelerateFrameCount
 local onTargetX, onTargetY, onTargetXY -- graphics that become visible when on target
+local circle -- represents target orbit in round 2 of thrustNav
+local earthHasCollided = false -- flag for taunts to show one time only
+local sunHasCollided = false   -- flag for taunts to show one time only
 
 -- Make a small red circle centered at the given location
 local function makeStar( x, y )
@@ -265,16 +277,18 @@ function act:prepare()
 		xVelocity = 0
 		printPositions()
     	-- put a circle around big Mars
-   		local xCenter = mars.x
-		local yCenter = mars.y
-		local c = display.newCircle( spaceGroup, xCenter, yCenter, ( mars.width * 4 ) +  mars.width / 1.6 )
-		print( "xCenter=", xCenter,"  yCenter=", yCenter )
+   		-- local xCenter = mars.x
+		-- local yCenter = mars.y
+		circle:scale( 8, 8 )
+		-- circle = display.newCircle( spaceGroup, xCenter, yCenter, ( mars.width * 4 ) +  mars.width / 1.6 )
+		print( "xCenter=", circle.x,"  yCenter=", circle.y )
 		print( "mars.width= ", mars.width )
 		print( "xmin=", mars.contentBounds.xMin, " xMax=", mars.contentBounds.xMax, 
 			" yMin=", mars.contentBounds.yMin, " yMax=", mars.contentBounds.yMax )
-		c.strokeWidth = 2
-		c:setStrokeColor( 0, 1, 0 ) 
-		c:setFillColor( 0, 0, 0, 0.2 )
+		circle.strokeWidth = 2
+		circle:setStrokeColor( 0, 1, 0 ) 
+		circle:setFillColor( 0, 0, 0, 0.2 )
+		circle.isVisible = true
     	if( game.cheatMode ) then		-- move spaceGroup to final position	
 			spaceGroup.x = act.width * ( -0.09375 )  --   -30 works for iphone5
     		spaceGroup.y = act.height * 0.4128 -- 165 works for iphone 5
@@ -336,7 +350,7 @@ function act:init()
 	bgLeft = display.newImageRect( spaceGroup, "media/thrustNav/starrynight.png", 2*act.width, 2*act.height )
 	bgLeft.x = act.xCenter
 	bgLeft.y = act.yCenter
-	bgRight = display.newImageRect( spaceGroup, "media/thrustNav/starrynight2.png", 2*act.width, 2*act.height )
+	bgRight = display.newImageRect( spaceGroup, "media/thrustNav/starrynight.png", 2*act.width, 2*act.height )
 	bgRight.x = act.xCenter * 5
 	bgRight.y = act.yCenter
 
@@ -362,6 +376,10 @@ function act:init()
     mars.y = act.yMin + act.height * 0.08803
     print("Mars anchorX=", mars.anchorX, "anchorY=", mars.anchorY )
     print("Mars is placed at ", mars.x, ",", mars.y , " in spaceGroup before move") 
+
+	-- create circle for target orbit and hide it
+	circle = display.newCircle( spaceGroup, mars.x, mars.y, ( mars.width * 4 ) +  mars.width / 1.6 )
+	circle.isVisible = false
 
     earth = display.newImageRect( spaceGroup, "media/thrustNav/earth.png", 30, 30 )
     earth.x = mars.x + ( 2 * act.width )
@@ -496,18 +514,12 @@ function updateNavStats()
 	if( math.abs( xTargetDelta ) < 3  ) then 
 		xStr = xStr .. " On Target" 
 		onTargetX.isVisible = true
-	elseif( math.abs( xTargetDelta ) < 6  ) then 
-		xStr = xStr .. " Getting close" 
-		onTargetX.isVisible = true
 	else
 		onTargetX.isVisible = false
 		onTargetXY.isVisible = false
 	end
 	if( math.abs( yTargetDelta ) < 3  ) then 
 		yStr = yStr .. " On Target" 
-		onTargetY.isVisible = true
-	elseif( math.abs( yTargetDelta ) < 6  ) then 
-		yStr = yStr .. " Getting close" 
 		onTargetY.isVisible = true
 	else
 		onTargetY.isVisible = false
@@ -538,16 +550,18 @@ function updateNavStats()
 		game.saveState.thrustNav.onTarget = true
 	end
 
-	if( hasCollided( earth, targetRect ) ) then
-		game.messageBox( "Are you going Home!?!"  )
-		navStatsText1.text = "Where are you going?  Home?"	
-	elseif( hasCollided( sun, targetRect ) ) then
-		navStatsText1.text = "That will be VERY HOT!"
+	if( hasCollided( earth, targetRect )  and earthHasCollided == false ) then
+		game.messageBox( "Are you going Home!?!" )
+		-- navStatsText1.text = "Where are you going?  Home?"	
+		earthHasCollided = true
+	elseif( hasCollided( sun, targetRect ) and sunHasCollided == false ) then
+		-- navStatsText1.text = "That will be VERY HOT!"
 		game.messageBox( "That will be VERY HOT!!"  )	
+		sunHasCollided = true
 	else
-		navStatsText1.text = string.format( "%s  %3d %5.1f   %s", "xDelta=", xTargetDelta , xVelocity, xStr)
-		navStatsText2.text = string.format( "%s  %3d %5.1f   %s", "yDelta=", yTargetDelta , yVelocity, yStr)
-		navStatsText3.text = string.format( "%s %4d", "Total Impulses= ", totalRocketImpulses )
+		navStatsText1.text = string.format( "                       %+6s %+5s", "Angle", "Vel"  )
+		navStatsText2.text = string.format( "%+12s  %+8d %+7.1f", "Horizontal=", xTargetDelta , xVelocity )
+		navStatsText3.text = string.format( "%+12s  %+8d %+7.1f", " Vertical    =", yTargetDelta , yVelocity )
 	end
 end
 
@@ -607,6 +621,7 @@ function updatePosition()
 		if( hasCollided( bgRight, mars ) ) then   -- move mars as needed
 			print( "move mars with right tile to left")
 			mars.x = mars.x - ( 4 * act.width )
+			circle.x = circle.x - ( 4 * act.width )  -- move circle with mars
 		end
 		if( hasCollided( bgRight, earth ) ) then   -- move earth as needed
 			print( "move Earth with right tile to left")
@@ -625,6 +640,7 @@ function updatePosition()
 		if( hasCollided( bgLeft, mars ) ) then  -- move mars as needed
 			print( "Move Mars with left tile to right")
 			mars.x = mars.x + ( 4 * act.width )
+			circle.x = circle.x + ( 4 * act.width )  -- move circle with mars
 		end
 		if( hasCollided( bgLeft, earth ) ) then  -- move earth as needed
 			print( "Move Earth with left tile to right")
@@ -658,6 +674,7 @@ end
 -- print debug positon information to console
 function printPositions()
 	print("Mars  x=", mars.x, "  y=", mars.y, "  ax=", mars.anchorX, "  ay=", mars.anchorY )
+	print("Circle  x=", circle.x, "  y=", circle.y, "  ax=", circle.anchorX, "  ay=", circle.anchorY )
 	print("Mars  contentBounds.xMin=", mars.contentBounds.xMin, "  Mars cb.xMax=", mars.contentBounds.xMax )
 	print("Mars  contentBounds.yMin=", mars.contentBounds.yMin, "  Mars cb.yMax=", mars.contentBounds.yMax )
 	print("Space x=", spaceGroup.x, "  y=", spaceGroup.y, "  ax=", spaceGroup.anchorX, "  ay=", spaceGroup.anchorY )
